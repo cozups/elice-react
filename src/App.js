556723c05571e3f1ab684c5b40a97a6f8b79adc6
Kim Miso <kimms9725@naver.com>
2022-06-06 13:28:33 +0900

    build

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return (
+    <header>
+      <h1>
+        <a href="/">Web</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB!
+    </article>
+  );
+}
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <Article />
+    </div>
+  );
 }
 
 export default App;
