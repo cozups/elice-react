8deca281bc3f3c4eea02254a120b3e7488b22e79
Kim Miso <kimms9725@naver.com>
2022-06-08 14:46:15 +0900

    mui & useState

--- src/App.js
+++ src/App.js
@@ -1,46 +1,103 @@
 import logo from './logo.svg';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
 import './App.css';
 
-function Header() {
+function Header(props) {
   return (
     <header>
       <h1>
-        <a href="/">Web</a>
+        <a
+          href="/"
+          onClick={(evt) => {
+            evt.preventDefault();
+            props.onSelect();
+          }}
+        >
+          Web
+        </a>
       </h1>
     </header>
   );
 }
 
-function Nav() {
+function Nav(props) {
+  const list = props.data.map((e) => (
+    <li key={e.id}>
+      <a
+        href={'/read/' + e.id}
+        onClick={(evt) => {
+          evt.preventDefault();
+          props.onSelect(e.id);
+        }}
+      >
+        {e.title}
+      </a>
+    </li>
+  ));
+
   return (
     <nav>
-      <ol>
-        <li>
-          <a href="/read/1">html</a>
-        </li>
-        <li>
-          <a href="/read/2">css</a>
-        </li>
-      </ol>
+      <ol>{list}</ol>
     </nav>
   );
 }
 
-function Article() {
+function Article(props) {
   return (
     <article>
-      <h2>Welcome</h2>
-      Hello, WEB!
+      <h2>{props.title}</h2>
+      {props.body}
     </article>
   );
 }
 
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+  const topics = [
+    { id: 1, title: 'html', body: 'html is ...' },
+    { id: 2, title: 'css', body: 'css is ...' },
+  ];
+
+  let content = null;
+  if (mode === 'WELCOME') {
+    content = <Article title="Welcome" body="Hello, WEB!" />;
+  } else if (mode === 'READ') {
+    const topic = topics.filter((e) => e.id === id)[0];
+    content = <Article title={topic.title} body={topic.body} />;
+  }
   return (
     <div>
-      <Header />
-      <Nav />
-      <Article />
+      <Header
+        onSelect={() => {
+          setMode('WELCOME');
+        }}
+      />
+      <Nav
+        data={topics}
+        onSelect={(id) => {
+          setMode('READ');
+          setId(id);
+        }}
+      />
+      {content}
+      <ButtonGroup
+        variant="outlined"
+        aria-label="outlined primary button group"
+      >
+        <Button
+          onClick={() => {
+            alert('create!');
+          }}
+        >
+          Create
+        </Button>
+        <Button>Update</Button>
+      </ButtonGroup>
+      <Button variant="outlined">Delete</Button>
     </div>
   );
 }
