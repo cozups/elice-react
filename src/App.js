a802d9aec04a7d8593b0f1070f4378a2d6f4442c
Kim Miso <kimms9725@naver.com>
2022-06-14 13:55:06 +0900

    0613

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
+import './App.css';
 import { useState } from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
-
-import './App.css';
+import styled from 'styled-components';
 
 function Header(props) {
   return (
-    <header>
+    <header className={props.className}>
       <h1>
         <a
           href="/"
@@ -16,34 +16,17 @@ function Header(props) {
             props.onSelect();
           }}
         >
-          Web
+          WWW
         </a>
       </h1>
     </header>
   );
 }
 
-function Nav(props) {
-  const list = props.data.map((e) => (
-    <li key={e.id}>
-      <a
-        href={'/read/' + e.id}
-        onClick={(evt) => {
-          evt.preventDefault();
-          props.onSelect(e.id);
-        }}
-      >
-        {e.title}
-      </a>
-    </li>
-  ));
-
-  return (
-    <nav>
-      <ol>{list}</ol>
-    </nav>
-  );
-}
+const HeaderStyled = styled(Header)`
+  border-bottom: 1px solid gray;
+  color: red;
+`;
 
 function Article(props) {
   return (
@@ -53,51 +36,130 @@ function Article(props) {
     </article>
   );
 }
-
+function Nav(props) {
+  const liTags = props.data.map((e) => {
+    return (
+      <li key={e.id}>
+        <a
+          href={'/read/' + e.id}
+          onClick={(evt) => {
+            evt.preventDefault();
+            props.onSelect(e.id);
+          }}
+        >
+          {e.title}
+        </a>
+      </li>
+    );
+  });
+  return (
+    <nav>
+      <ol>{liTags}</ol>
+    </nav>
+  );
+}
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form
+        onSubmit={(evt) => {
+          evt.preventDefault();
+          const title = evt.target.title.value;
+          const body = evt.target.body.value;
+          props.onCreate(title, body);
+        }}
+      >
+        <p>
+          <input type="text" name="title" placeholder="title"></input>
+        </p>
+        <p>
+          <textarea name="body" placeholder="body"></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Create"></input>
+        </p>
+      </form>
+    </article>
+  );
+}
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(3);
+  const [topics, setTopics] = useState([
     { id: 1, title: 'html', body: 'html is ...' },
     { id: 2, title: 'css', body: 'css is ...' },
-  ];
-
+  ]);
   let content = null;
   if (mode === 'WELCOME') {
-    content = <Article title="Welcome" body="Hello, WEB!" />;
+    content = <Article title="Welcome" body="Hello, WEB!"></Article>;
   } else if (mode === 'READ') {
-    const topic = topics.filter((e) => e.id === id)[0];
-    content = <Article title={topic.title} body={topic.body} />;
+    const topic = topics.filter((e) => {
+      if (e.id === id) {
+        return true;
+      } else {
+        return false;
+      }
+    })[0];
+    content = <Article title={topic.title} body={topic.body}></Article>;
+  } else if (mode === 'CREATE') {
+    content = (
+      <Create
+        onCreate={(title, body) => {
+          const newTopic = { id: nextId, title, body };
+          const newTopics = [...topics];
+          newTopics.push(newTopic);
+          setTopics(newTopics);
+          setId(nextId);
+          setMode('READ');
+          setNextId(nextId + 1);
+        }}
+      ></Create>
+    );
   }
   return (
     <div>
-      <Header
+      <HeaderStyled
         onSelect={() => {
           setMode('WELCOME');
         }}
-      />
+      ></HeaderStyled>
       <Nav
         data={topics}
         onSelect={(id) => {
           setMode('READ');
           setId(id);
         }}
-      />
+      ></Nav>
       {content}
-      <ButtonGroup
-        variant="outlined"
-        aria-label="outlined primary button group"
-      >
+      <ButtonGroup>
         <Button
+          variant="outlined"
           onClick={() => {
-            alert('create!');
+            setMode('CREATE');
           }}
         >
           Create
         </Button>
-        <Button>Update</Button>
+        <Button variant="outlined">Update</Button>
       </ButtonGroup>
-      <Button variant="outlined">Delete</Button>
+      <Button
+        variant="outlined"
+        onClick={() => {
+          const newTopics = topics.filter((e) => {
+            if (e.id === id) {
+              return false;
+            } else {
+              return true;
+            }
+          });
+          setMode('WELCOME');
+          setTopics(newTopics);
+        }}
+      >
+        Delete
+      </Button>
     </div>
   );
 }
